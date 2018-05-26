ffc359a1fb7950e0cb106f86b3c30c9499f75038
fassler <fasslero@gmail.com>
2018-05-26 12:44:29 +0300

    WIP

--- dflow_calc.cpp
+++ dflow_calc.cpp
@@ -1,8 +1,9 @@
 #include "dflow_calc.h"
+#include <vector>
 typedef std::vector<Node&> NodeVec;
 typedef std::vector<Node&>::iterator NodeVecIterator;
-#define EXIT -2;
-#define ENTRY -1;
+#define EXIT -2
+#define ENTRY -1
 
 /*************************************!
  * Node class
@@ -19,7 +20,7 @@ class Node{
 
 public:
     ~Node();
-    void getParents(int parent1, int parent2);
+    void getParents(int* parent1, int* parent2);
     int getDepth();
     int getLatency();
     int getNum();
@@ -54,7 +55,8 @@ Node::Node (InstInfo* nodeInstructionInfo, int commandNum){
 Node::Node(int type){
 
     if (type == EXIT) {
-        //create exit node todo - something is wrong here
+        // create exit node
+        // todo - I think we can use use the node object by reverse (successors vector will be the parent vector)
     }
     else if (type == ENTRY){
         //create entry node
@@ -74,7 +76,7 @@ Node::~Node(){
  * Add the given node to the successor node vector.
  */
 void Node::updateSuccessor(Node &node) {
-    successors.push(node); //todo - is push the right method?
+    successors.push_back(node); //todo - is push_back the right method?
 }
 
 /*!
@@ -110,7 +112,7 @@ void Node::getParents(int* parent1Num, int* parent2Num) {
  * program class
  ****************************************/
 class program{
-    Node* registerToNode[MAX_OPS];   //change the name to something "last command that touched the register"
+    Node* registerToNode[MAX_OPS];   // todo - change the name to something "last command that touched the register"
     NodeVec allNodesVector;
     InstInfo progTrace[];
     unsigned int numOfInsts;
@@ -140,7 +142,9 @@ program::~program() {}
 /*!
  * returns the program maxDepth (longest trail from exit to entry in sec)
  */
-int program::getMaxDepth() {}
+int program::getMaxDepth() {
+    return maxDepth;
+}
 
 /*!
  * returns the depth of a certain node
@@ -152,7 +156,10 @@ int program::getNodeDepth(unsigned int commandNumber) {
 /*!
  * save the nodes parent numbers in the pointers provided, and return 0 for success or -1 if fail
  */
-int program::getNodeParents(unsigned int commandNumber, int *parent1CommandNum, int *parent1CommandNum) {}
+int program::getNodeParents(unsigned int commandNumber, int *parent1CommandNum, int *parent2CommandNum) {
+    allNodesVector[commandNumber].getParents(parent1CommandNum, parent2CommandNum);
+    return 0;
+}
 
 /*!
  * gets a register src idx and returns the latest command that write to that register
@@ -163,23 +170,31 @@ Node& program::searchForPredecessor(int srcIdx){
 }
 
 
-
+/***************************************!
+ * Interface functions
+ ***************************************/
 ProgCtx analyzeProg(const unsigned int opsLatency[],  InstInfo progTrace[], unsigned int numOfInsts) {
-    return PROG_CTX_NULL;
+    ProgCtx program = new program(opsLatency, progTrace, numOfInsts);
+    return program;
+
+    // todo - need this? return PROG_CTX_NULL;
 }
 
 void freeProgCtx(ProgCtx ctx) {
+    delete ctx;
 }
 
 int getInstDepth(ProgCtx ctx, unsigned int theInst) {
-    return -1;
+    return ctx.getNodeDepth(theInst);
 }
 
 int getInstDeps(ProgCtx ctx, unsigned int theInst, int *src1DepInst, int *src2DepInst) {
-
-    return -1;
+    ctx.getNodeParents(theInst,src1DepInst,src2DepInst);
+    return 0;
+    // return -1 if fail, how can it fail?
 }
 
 int getProgDepth(ProgCtx ctx) {
+    ctx.getMaxDepth();
     return 0;
 }
